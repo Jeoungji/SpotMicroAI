d556d411e38f9198c4f6f274990fba3ea5a3febf
Jiu Jeong <dnw0406@naver.com>
2024-03-20 14:04:54 +0900

    Fix Average Filter
    - Fix Error of Average Filter
    - Delete Filter.cpp file

--- src/Filter.h
+++ src/Filter.h
@@ -1,17 +1,50 @@
 #pragma once
+
+#ifndef _FILTER
+#define _FILTER
+
 #include <Arduino.h>
+#define AVERAGEFILTERSIZE 200
 
 class MovingAverageFilter{
 private:
     const uint16_t size;
-    float datas[sizeof(size)];
+    float datas[AVERAGEFILTERSIZE];
     uint16_t head;
     uint16_t curretdatasize;
     double sum;
 public:
-    MovingAverageFilter(uint16_t size_);
-    void PutData(float data);
-    void ClearData();
-    float GetData();
+    MovingAverageFilter() 
+    : size(AVERAGEFILTERSIZE-1), head(0), curretdatasize(0), sum(0){    }
+    void PutData(float data_)
+    {
+        datas[head] = data_;
+
+        if (curretdatasize < size)
+        curretdatasize += 1; 
+
+        sum = sum + (double)data_;
+
+        if (head < size )
+        head += 1;
+        else 
+        head = 0;
+
+        if (curretdatasize == size)
+            sum = sum - (double)datas[head];
+    }
+    void ClearData() {
+        curretdatasize = 0;
+        head = 0;
+    }   
+
+    float GetData() { return (float)(sum/curretdatasize); }
+
+    bool Full() {
+        if (curretdatasize == size) return true;
+        return false;
+    }
+    uint16_t MovingAverageFilter::Size() { return curretdatasize; }
 };
 
+#endif
